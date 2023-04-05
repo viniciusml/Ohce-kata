d899c8d5d571474c27993f2bcd76ff3825238f9c
Vinicius Leal <viniciusmoreira.leal@thetrainline.com>
2023-04-05 20:06:49 +0100

    Run exits with error code when no arguments are passed

--- Tests/ohceTests/AcceptanceCriteriaTests.swift
+++ Tests/ohceTests/AcceptanceCriteriaTests.swift
@@ -1,8 +1,47 @@
 //
-//  File.swift
+//  AcceptanceCriteriaTests.swift
 //  
 //
 //  Created by Vinicius Leal on 05/04/2023.
 //
 
-import Foundation
+import ohce
+import XCTest
+
+final class AcceptanceCriteriaTests: XCTestCase {
+    
+    func test_run_withNoArguments_exitsAppWithError() {
+        expectExit(expectedCode: 1) {
+            let sut = Ohce()
+            
+            sut.run(nil)
+        }
+    }
+}
+
+extension XCTestCase {
+    func expectExit(expectedCode: Int32, testcase: @escaping () -> Void) {
+        let exp = expectation(description: "expecting Exit")
+        var errorCode: Int32? = nil
+        
+        ExitUtil.replaceExit { code in
+            errorCode = code
+            exp.fulfill()
+            self.unreachable()
+            
+        }
+        DispatchQueue.global(qos: .userInitiated).async(execute: testcase)
+        
+        waitForExpectations(timeout: 0.5) { _ in
+            XCTAssertEqual(errorCode, expectedCode)
+            
+            ExitUtil.restoreExit()
+        }
+    }
+    
+    private func unreachable() -> Never {
+        repeat {
+            RunLoop.current.run()
+        } while (true)
+    }
+}
