4dac88299a848d13019e3c08f3e391cd4a2715f6
Vinicius Leal <viniciusmoreira.leal@thetrainline.com>
2023-04-09 13:10:38 +0100

    Use argument processor in main

--- Sources/ohce/main.swift
+++ Sources/ohce/main.swift
@@ -3,4 +3,12 @@ import Foundation
 let printer = Printer()
 let exiter = Exiter()
 let app = Ohce(printer: printer, exiter: exiter)
-app.run(nil)
+
+let argumentProcessor = ArgumentProcessor(
+    onInvalidArgument: {
+        // TODO: move error handling
+    },
+    onValidArgument: { argument in
+        app.run(argument)
+    }
+)
